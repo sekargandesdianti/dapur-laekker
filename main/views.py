2a6935d2b4605b642c8907c63c1ecd88e3b09c43
sekargandesdianti <sekargandesdianti@gmail.com>
2023-09-12 06:37:07 +0700

    Memperbaiki kesalahan penulisan kelas PBP

--- main/views.py
+++ main/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Sekar Gandes Dianti',
-        'class': 'PBP C'
+        'class': 'PBP D'
     }
     return render(request, "main.html", context)
